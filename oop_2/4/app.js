3dc00f60c07022258741ef56e3f89e4fc711715d
VladislavGazuk <vladislav.gazuk@gmail.com>
2022-10-19 14:50:49 +0200

    add 4-5 tasks

--- oop_2/4/app.js
+++ oop_2/4/app.js
@@ -1,1 +1,19 @@
 // Реализуйте класс Word, и функцию isPalindrome которая будет выводить в alert соответствующее будевое значение. Если значения 2 инпутов являются палиндромом -> true, в противном случае false
+
+class Word {
+    constructor() {
+        this.isPalindrome();
+    }
+    
+    isPalindrome() {
+        document.querySelector('.btn').addEventListener('click', () => {
+            const inpt1 = document.querySelector('.inpt1').value;
+            const inpt2 = document.querySelector('.inpt2').value;
+
+            if (inpt1 === inpt1.split('').reverse().join('') && inpt2 === inpt2.split('').reverse().join('')) alert(true);
+            else alert(false);
+        });
+    }
+
+}
+const word = new Word();
--- oop_2/5/app.js
+++ oop_2/5/app.js
@@ -1,1 +1,17 @@
 // Реализуйте класс Word, и функцию isAnagram которая будет выводить в alert соответствующее будевое значение. Если значения 2 инпутов являются анаграммами -> true, в противном случае false
+
+class Word {
+    constructor() {
+        this.isAnagram();
+    }
+
+    isAnagram() {
+        document.querySelector('.btn').addEventListener('click', () => {
+            const inpt1 = document.querySelector('.inpt1').value;
+            const inpt2 = document.querySelector('.inpt2').value;
+            if (inpt1.split('').sort().join('') === inpt2.split('').sort().join('')) alert(true);
+            else alert(false);
+        });
+    }
+}
+const word = new Word();
